45781a0f43f9f3a43c331ef9d00f9513822e32cd
Heiko Braun <ike.braun@googlemail.com>
2015-06-23 15:24:42 +0200

    link joda time for testing; extend tet coverage to deminstrate low level apis

--- src/test/java/org/wildfly/metrics/logstore/LogStoreTest.java
+++ src/test/java/org/wildfly/metrics/logstore/LogStoreTest.java
@@ -28,13 +28,17 @@ import jetbrains.exodus.entitystore.StoreTransaction;
 import jetbrains.exodus.entitystore.StoreTransactionalComputable;
 import jetbrains.exodus.entitystore.StoreTransactionalExecutable;
 import org.jetbrains.annotations.NotNull;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author Heiko Braun
@@ -48,8 +52,14 @@ public class LogStoreTest {
     private PersistentEntityStoreImpl store;
     private EntityId metricId;
 
+    static DateTimeFormatter FMT= DateTimeFormat.forPattern("MM/dd/yyyy HH:mm:ss");
+
+    /**
+     * Initialize a store at 'java.io.tmpdir' and create an example top level entity
+     * to record the measurements
+     */
     @Before
-    public void initializeStore() throws Exception {
+    public void initializeStore() {
         String tmpdir = System.getProperty("java.io.tmpdir");
         String dataDir = tmpdir + File.pathSeparator + "metrics-data";
 
@@ -80,8 +90,11 @@ public class LogStoreTest {
             store.close();
     }
 
+    /**
+     * Insert N samples and make sure they can be read again.
+     */
     @Test
-    public void testReadWrite() throws Exception {
+    public void testReadWrite() {
 
         final int NUM_RECORDS = 5;
 
@@ -91,15 +104,10 @@ public class LogStoreTest {
                     @Override
                     public void execute(@NotNull StoreTransaction txn) {
                         Entity metric = txn.getEntity(metricId);
-
-                        for(int i=0; i<NUM_RECORDS; i++) {
-                            double random = Math.random();
-                            double probe = random *100;
-                            int heap = (int)probe;
-                            Entity sample = createNewSample(txn, System.currentTimeMillis(), heap);
-                            metric.addLink("samples", sample);
+                        List<Entity> timeSeries = createTimeSeries(txn, NUM_RECORDS);
+                        for (Entity entry : timeSeries) {
+                            metric.addLink("samples", entry);
                         }
-
                     }
                 }
         );
@@ -126,8 +134,75 @@ public class LogStoreTest {
 
     }
 
+    @Test
+    public void testSlices() {
+
+        final int PAST_SECONDS = 60; // 60 seconds backwards
+
+        store.executeInTransaction(
+                new StoreTransactionalExecutable() {
+                    @Override
+                    public void execute(@NotNull StoreTransaction txn) {
+                        Entity metric = txn.getEntity(metricId);
+                        List<Entity> timeSeries = createTimeSeries(txn, PAST_SECONDS);
+                        for (Entity entry : timeSeries) {
+                            metric.addLink("samples", entry);
+                        }
+                    }
+                }
+        );
+
+        // time window
+        DateTime dt = new DateTime();
+        final DateTime from = dt.minusSeconds(30);
+        final DateTime to = dt.minusSeconds(15);
+
+        // read the previously created sample
+        store.executeInTransaction(
+                new StoreTransactionalExecutable() {
+                    @Override
+                    public void execute(@NotNull StoreTransaction txn) {
+                        Entity metric = txn.getEntity(metricId);
+                        //Entity samples = metric.getLink(SAMPLE_NAME);
+
+                        EntityIterable slice = txn.find(SAMPLE_NAME, "timestamp", from.getMillis(), to.getMillis());
+
+                        int i=0;
+                        for (Entity sample : slice) {
+                            printSample(sample);
+                            i++;
+                        }
+
+                        Assert.assertEquals("Expected 15 records within slice", 15, i);
+
+                    }
+
+                }
+        );
+    }
+
+    private static List<Entity> createTimeSeries(StoreTransaction txn, int size) {
+
+        List<Entity> results = new LinkedList<>();
+
+        DateTime dt = new DateTime();
+        DateTime offset = dt.minusSeconds(size);
+
+        for(int i=0; i<size; i++) {
+            double random = Math.random();
+            double probe = random *100;
+            int heap = (int)probe;
+            offset = offset.plusSeconds(1);
+            Entity sample = createNewSample(txn, offset.getMillis(), heap);
+            results.add(sample);
+        }
+
+        return results;
+    }
+
     private static void printSample(Entity item) {
-        System.out.println("\tTimestamp: " + item.getProperty("timestamp"));
+        DateTime timestamp = new DateTime(item.getProperty("timestamp"));
+        System.out.println("\tTimestamp: " + FMT.print(timestamp));
         System.out.println("\tValue: " + item.getProperty("value"));
     }
 
