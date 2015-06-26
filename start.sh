652c0f6a2e545abf7521b4175dfc108014289a55
Heiko Braun <ike.braun@googlemail.com>
2015-06-26 11:06:15 +0200

    cleanup

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export MAVEN_OPTS="-agentpath:/Applications/jprofiler7/bin/macos/libjprofilerti.jnilib=port=8849,nowait"
-mvn exec:java -Dexec.mainClass="org.wildfly.metrics.server.Server" 
-
